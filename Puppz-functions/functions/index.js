8b77ee84afe7d1f56b9a0657b7460cf2eb1c5218
Jeffrey Platt <jsplatt@gmail.com>
2021-07-06 19:04:37 -0400

    DL postman desktop allowed access

--- Puppz-functions/functions/index.js
+++ Puppz-functions/functions/index.js
@@ -1,6 +1,39 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin')
+
+admin.initializeApp()
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello World!");
 });
+
+exports.getWoofs = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('woofs').get()
+        .then(data => {
+            let woofs = []
+            data.forEach(doc => {
+                woofs.push(doc.data())
+            })
+            return res.json(woofs)
+        })
+            .catch(err => console.error(err))
+})  
+
+exports.createWoof = functions.https.onRequest((req, res) => {
+    const newWoof = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    }
+    admin.firestore()
+        .collection('woofs')
+        .add(newWoof)
+        .then(doc => {
+            res.json({message: `document ${doc.id} created successfully`})
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong' })
+            console.error(err)
+        })  
+})
