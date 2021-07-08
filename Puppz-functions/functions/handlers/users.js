b54ca072d43fbecc1dd5a46408ad72f52c32e365
Jeffrey Platt <jsplatt@gmail.com>
2021-07-08 14:02:03 -0400

    figuring it out

--- Puppz-functions/functions/handlers/users.js
+++ Puppz-functions/functions/handlers/users.js
@@ -5,7 +5,7 @@ const config = require('../util/config')
 const firebase = require ('firebase')
 firebase.initializeApp(config)
 
-const { validateSignupData, validateLogInData } = require('../util/validators')
+const { validateSignupData, validateLoginData } = require('../util/validators')
 
 exports.signup = (req,res) => {
     const newUser = {
@@ -86,6 +86,11 @@ exports.login = (req, res) => {
         })
 }
 
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body)
+}
+
+
 exports.uploadImage = (req, res) => {
     const BusBoy = require('busboy')
     const path = require('path')
@@ -98,9 +103,9 @@ exports.uploadImage = (req, res) => {
     let imageToBeUploaded = {}
 
     busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    console.log(fieldname)
-    console.log(filename)
-    console.log(mimetype)
+    if(mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+        return res.status(400).json({ error: "Wrong file type submitted"})
+    }
 
         const imageExtension = filename.split('.')[filename.split('.').length - 1]
         const imageFileName = `${Math.round(Math.random()*100000000000)}.${imageExtension}`
--- Puppz-functions/functions/index.js
+++ Puppz-functions/functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')()
 const FBAuth = require ('./util/fbAuth')
 
 const { getAllWoofs, postOneWoof } = require('./handlers/woofs')
-const { signup, login, uploadImage } = require('./handlers/users')
+const { signup, login, uploadImage, addUserDetails } = require('./handlers/users')
 
 // Woof Routes
 app.get('/woofs', getAllWoofs)
@@ -15,5 +15,6 @@ app.post('/woof',FBAuth, postOneWoof)
 app.post('/signup', signup)
 app.post('/login', login)
 app.post('/user/image', FBAuth, uploadImage)
+app.post('/user', FBAuth, addUserDetails)
 
 exports.api = functions.https.onRequest(app)
